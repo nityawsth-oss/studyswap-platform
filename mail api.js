abaa28c1c77677671b60d1e09235fb8b816433e2
nityawsth-oss <nityawsth@gmail.com>
2026-04-01 20:12:52 +0530

    Create mail api.js

---
+++ mail api.js
@@ -0,0 +1,7 @@
+<form>
+  <label for="email">Google Email:</label>
+  <input type="email" id="email" name="email" 
+         pattern="[a-z0-9._%+-]+@gmail\.com$" 
+         required>
+  <button type="submit">Submit</button>
+</form>
